671289fdd1d2643cfd9896d2d9d9e081d940b7d1
Daniela <danielaaldaco9@gmail.com>
2024-09-04 15:35:11 -0600

    Proyecto inicial

---
+++ src/operacion.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class operacion {
+    private JPanel pnlOperaciones;
+    private JTextField textDigito1;
+    private JTextField textDigito2;
+    private JButton sumarButton;
+    private JButton restarButton;
+    private JButton multiplicarButton;
+    private JButton dividirButton;
+}
